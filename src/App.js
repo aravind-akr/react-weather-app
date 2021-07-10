dac78386459f1cd7a1ffe901f57d70c8c8a883a2
Aravind <kraravind1993@gmail.com>
2021-07-10 13:08:33 +0530

    Final Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CityComponent from './modules/CityComponent';
+import WeatherComponent from './modules/WeatherInfoComponent';
+import axios from "axios";
 
+const API_KEY = "3def4122ad245aa05fa53f9b56071b7d";
+
+const Container = styled.div `
+  display:flex;
+  flex-direction:column;
+  margin:auto;
+  align-items:center;
+  box-shadow: 0 3px 6px #555;
+  padding:20px 10px;
+  border-radius: 4px;
+  width: 380px;
+  background: white;
+  font-family: Montserrat;
+`;
+
+const AppLabel = styled.span `
+  color:black;
+  font-size: 18px;
+  font-weight: bold;
+`;
 
 function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = 
+    await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
+    updateWeather(response.data);
+  };
   return (
-    <div>
-      AKR Weather App
-    </div>
+    <Container>
+    <AppLabel> React Weather App </AppLabel>
+      {weather? (
+        <WeatherComponent weather={weather}/>
+        ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather}/>
+        )}
+    </Container>
   );
 }
 
